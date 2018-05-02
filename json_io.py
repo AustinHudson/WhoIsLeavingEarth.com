cdbcfead8243a831c7eda091bc1afe687ffe66ec
AustinHudson <hudsaust@oregonstate.edu>
2018-05-02 13:04:48 -0500

    changed routes

--- json_io.py
+++ json_io.py
@@ -4,16 +4,16 @@ from spaceTracker import simple_get, getDateInfo, getLaunchInfo
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
 
-    return render_template("landingPage.html")
+    return render_template("index.html")
 
 
 @app.route("/missions")
 def output():
 
-    return render_template("index.html", dateList=getDateInfo())
+    return render_template("missions.html", dateList=getDateInfo())
 
 
 if __name__ == "__main__":
--- spaceTracker.py
+++ spaceTracker.py
@@ -2,10 +2,6 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-import csv
-
-
-
 
 
 def simple_get(url):
